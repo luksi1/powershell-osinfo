602d6a6b92c333397b968b7414246d701db7d039
Luke Simmons <luke.simmons@gmail.com>
2018-09-13 22:38:12 +0200

    added complete pester tests

--- OSInfo.Test.ps1
+++ OSInfo.Test.ps1
@@ -1,45 +1,74 @@
 Import-Module -force ${PSScriptRoot}\OSInfo.psm1
 
 Describe 'OSInfo' {
-
-    Mock -CommandName Get-ADComputer -ModuleName OSInfo { return @{
-        Name = "Foo"
-    }}
     
-    Mock -CommandName New-CimSession -ModuleName OSInfo { return @{
-        Id = "1";
-        Name = "CimSession1";
-        InstanceId = "1234";
-        ComputerName = "Foo";
-        Protocol = "DCOM"
-    }}
+    Mock New-CimSession -ModuleName OSInfo -MockWith { New-MockObject -Type Microsoft.Management.Infrastructure.CimSession }
+    Mock Get-CimSession -ModuleName OSInfo -MockWith { New-MockObject -Type Microsoft.Management.Infrastructure.CimSession }
+    Mock Remove-CimSession -ModuleName OSInfo { $true }
 
-    Mock -CommandName Get-CimSession -ModuleName OSInfo { return @{
-        Id = "1";
-        Name = "CimSession1";
-        InstanceId = "1234";
-        ComputerName = "Foo";
-        Protocol = "DCOM"
-    }}
-
-    Context "Get Service Information" {
+    Context "get disk information" {
         
-        Mock -CommandName Get-CimInstance -ModuleName OSInfo { return @{
+        Mock -CommandName Get-CimInstance -ParameterFilter { $ClassName -eq "Win32_logicaldisk" } -ModuleName OSInfo  { @{
+            Size = 238713565184;
+            DeviceId = "C:";
+            Name = "Foo";
+            DriveType = 3;
+            ProviderName = $null;
+            VolumeName = "Windows";
+            FreeSpace = 147779387392;
+        }}
+
+        $disk = Get-DiskInformation -ComputerName "Foo"
+    
+        It "returns the correct disk size" {   
+            $disk.SizeMB | Should Be 227654.99609375
+        }
+        It "returns the correct free size in megabytes" {
+            $disk.FreeSizeMb | Should Be 140933.40625
+        }
+        It "return the correct percentage of free space" {
+            $disk.PercentFree | Should Be "61.91%"
+        }
+        It "return the correct computer name" {
+            $disk.ComputerName | Should Be "Foo"
+        }
+    }
+
+    Context "get service information" {
+        
+        Mock -CommandName Get-CimInstance -ModuleName OSInfo -ParameterFilter { $ClassName -eq "Win32_Service" } { @{
             ProcessId = 0;
             Name = "Foo";
             StartMode = "Automatic";
             State = "Running";
             Status = "OK";
-            ExitCode = 0;
             StartName = "company\foo";
             Description = "This is my service";
-            DisplayName = "Foo";
+            DisplayName = "Foo"
         }}
 
-        $services = Get-ServiceInformation -ComputerName "Foo"
-    
-        It "returns the correct values" {   
-            $services.DisplayName | Should Be "Foo"
+        $service = Get-ServiceInformation -ComputerName "Foo"
+
+        It "returns the correct computer name" {
+            $service.ComputerName | Should Be "Foo"
+        }
+        It "returns the correct display name" {
+            $service.DisplayName | Should Be "Foo"
+        }
+        It "returns the correct service name" {
+            $service.ServiceName | Should Be "Foo"
+        }
+        It "returns the correct startup type" {
+            $service.StartupType | Should Be "Automatic"
+        }
+        It "returns the correct service account" {
+            $service.ServiceAccount | Should Be "company\foo"
+        }
+        It "returns the correct state" {
+            $service.state | Should Be "Running"
+        }
+        It "returns the correct description" {
+            $service.description | Should Be "This is my service"
         }
     }
     
--- OSInfo.psm1
+++ OSInfo.psm1
@@ -20,32 +20,27 @@ function Get-ServiceInformation {
      [cmdletbinding()]
      param (
         [parameter(position=0, valuefrompipeline, valuefrompipelinebypropertyname)]
-        # [ArgumentCompleter({
-        #     foreach ( $computer in (Get-ADComputer -filter *).Name ) {
-        #         [System.Management.Automation.CompletionResult]::new($computer, $computer, [System.Management.Automation.CompletionResultType]::ParameterValue, $computer)
-        #     }
-        # })]
-        # [adcomputer()]
-        [string[]] $ComputerName = $env:COMPUTERNAME,
-        [parameter(valuefrompipeline, valuefrompipelinebypropertyname)]
-        [string[]] $ServiceName
+        [ArgumentCompleter({
+            foreach ( $computer in (Get-ADComputer -filter *).Name ) {
+                [System.Management.Automation.CompletionResult]::new($computer, $computer, [System.Management.Automation.CompletionResultType]::ParameterValue, $computer)
+            }
+        })]
+        [adcomputer()]
+        [string[]] $ComputerName=$env:COMPUTERNAME
      )
 
      # servicename, displayname, startuptype, serviceaccount
-     begin { New-CimSession -ComputerName $ComputerName }
+     begin { New-CimSession -ComputerName $ComputerName | Out-Null }
 
      process {
-
-         Get-CimSession | Get-CimInstance Win32_Service -PipelineVariable services | 
-         Select-Object -Property `
+         Get-CimSession | Get-CimInstance -ClassName Win32_Service | 
+         Select-Object -Property @{N='DisplayName'; E={$_.DisplayName}},
+            @{N='State'; E={$_.State}},
+            @{N='Description'; E={$_.Description}},
             @{N='ComputerName'; E={$ComputerName}},
-            DisplayName,
-            @{N='ServiceName'; E={$services.Name}}, 
-            @{N='StartupType'; E={$services.StartMode}}, 
-            @{N='ServiceAccount'; E={$services.StartName}},
-            Status,
-            State,
-            Description
+            @{N='ServiceName'; E={$_.Name}}, 
+            @{N='StartupType'; E={$_.StartMode}}, 
+            @{N='ServiceAccount'; E={$_.StartName}}
      }
 
      end { Get-CimSession | Remove-CimSession  }
@@ -68,7 +63,7 @@ function Get-DiskInformation {
      begin { New-CimSession -ComputerName $ComputerName | Out-Null }
 
      process {
-         Get-CimSession | Get-CimInstance Win32_logicaldisk | 
+         Get-CimSession | Get-CimInstance -ClassName Win32_logicaldisk | 
          Select-Object -Property Name, 
             @{N='SizeMB'; E={$_.Size/1mb}}, 
             @{N='FreeSizeMB'; E={$_.FreeSpace/1mb}},
